5f86b8c4d8b0b5733f758666cdfbed31ab6afdfb
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2021-12-11 18:55:28 +0000

    HIP-8: Added example module for testing purposes.

---
+++ hippo/example_module.py
@@ -0,0 +1,9 @@
+
+class ExampleClass:
+    """
+    This class is an example with a random docstring.
+
+    Attributes:
+        example_attribute (int): A random integer attribute
+    """
+    pass
