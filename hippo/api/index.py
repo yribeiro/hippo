3bae9a6faf3d50eddfaed6d5133fae898c86a126
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2022-01-16 14:58:32 +0000

    HIP-18: Updated bug in router definition - second time lucky.

--- hippo/api/index.py
+++ hippo/api/index.py
@@ -3,16 +3,16 @@ from typing import Dict
 from fastapi import APIRouter
 
 # Note: the prefix here indicates it is the root path i.e. http://service:port/
-router = APIRouter(tags=["index"], prefix="/")
+router = APIRouter(tags=["index"], prefix="")
 
 
-@router.get("")
+@router.get("/")
 def index_get() -> Dict[str, str]:
     """Exemplar GET endpoint for the API service. Simply returns a welcome message."""
     return {"message": "Welcome to the Hippo diagnosis service!"}
 
 
-@router.post("")
+@router.post("/")
 def index_post() -> Dict[str, str]:
     """Exemplar POST endpoint for the API service. Simply returns a welcome message."""
     return {"message": "Welcome to the Hippo diagnosis service!"}
