5e9c525093319a44893e238a7e31671b6221b67b
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2022-01-17 21:08:17 +0000

    HIP-18: Added unit tests for the index route.

--- tests/test_api/test_index.py
+++ tests/test_api/test_index.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
-class TestAPIIndexEndpoint:
-    def test_get_method_works_on_index_endpoint(self) -> None:
-        pass
+import pyexpat
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
-    def test_post_method_works_on_index_endpoint(self) -> None:
-        pass
+from hippo.main import DEVAPP
+
+import pytest
+
+
+class TestAPIIndexEndpoint:
+    """
+    Test suite to check the index GET and POST methods.
+
+    This test uses the FastAPI convention for testing routes. See the following link:
+    https://fastapi.tiangolo.com/tutorial/testing/
+    """
+
+    @pytest.mark.parametrize("test_method", ["get", "post"])
+    def test_get_method_works_on_index_endpoint(self, test_method: str) -> None:
+        # setup
+        client = TestClient(app=DEVAPP)
+
+        # execute
+        if test_method == "get":
+            resp = client.get("/")
+        else:
+            resp = client.post("/")
+
+        # assert
+        assert resp.status_code == 200
+        assert "Welcome" in resp.json()["message"]
+        assert "Hippo" in resp.json()["message"]
