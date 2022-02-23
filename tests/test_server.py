6c77219a57e59d47a6891e67416489241c6e0a8b
mateimol <matei.moldoveanu@gmail.com>
2022-02-23 21:59:58 +0100

    HIP-30: Test server initialises thread

--- tests/test_server.py
+++ tests/test_server.py
@@ -19,3 +19,15 @@ class TestServer:
         # test if the server class
         with pytest.raises(AssertionError):
             _ = Server(app=mockFastApi, host=test_host, port=test_port)
+
+    @patch("hippo.server.threading.Thread")
+    @patch("hippo.server.uvicorn.Config")
+    @patch("hippo.server.uvicorn.Server.run")
+    def test_server_initialises_config_and_thread_as_expected(
+        self, mockUvicornServer: Mock, mockUvicorn: Mock, mockThread: Mock
+    ) -> None:
+
+        Server(app="hippo.main:DEVAPP", host="localhost", port=12345, log_level="info")
+
+        mockUvicorn.assert_called_once_with("hippo.main:DEVAPP", host="localhost", port=12345, log_level="info")
+        mockThread.assert_called_once_with(target=mockUvicornServer, daemon=True)
